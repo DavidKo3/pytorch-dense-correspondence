50dd039b488029e3191ca98275d375bb7742db31
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2019-12-03 05:24:22 +0000

    Pushing with half-done implementation of lipschitz

--- dense_correspondence/loss_functions/loss_composer.py
+++ dense_correspondence/loss_functions/loss_composer.py
@@ -110,7 +110,56 @@ def distributional_loss_batch(image_a_pred, image_b_pred, matches_a, matches_b, 
     loss = F.kl_div(q_a.log(), p_a, None, None, 'sum')/matches_b_descriptor.shape[0]
     return loss
 
+def lipschitz_single(match_b, match_b2, image_a_pred, image_b_pred, L, d, image_width=640, image_height=480):
+    match_b_descriptor = torch.index_select(image_b_pred, 1, match_b) # get descriptor for image_b at match_b
+    norm_degree = 2
+    descriptor_diffs = image_a_pred.squeeze() - match_b_descriptor.squeeze()
+    norm_diffs = descriptor_diffs.norm(norm_degree, 1).pow(2)
+    best_match_idx_flattened = torch.argmin(norm_diffs) #Adi: do we need to change the dim?
+    u_b = best_match_idx_flattened%image_width
+    v_b = best_match_idx_flattened/image_width
+    unraveled = torch.stack((u_b, v_b)).type(torch.float64)
+    uv_b = Variable(unraveled.cuda().squeeze(), requires_grad=True)
+
+    match_b2_descriptor = torch.index_select(image_b_pred, 1, match_b2) # get descriptor for image_b at match_b
+    descriptor_diffs_2 = image_a_pred.squeeze() - match_b2_descriptor.squeeze()
+    norm_diffs_2 = descriptor_diffs_2.norm(norm_degree, 1).pow(2)
+    best_match_idx_flattened_2 = torch.argmin(norm_diffs_2) #Adi: do we need to change the dim?
+    u_b2 = best_match_idx_flattened_2%image_width
+    v_b2 = best_match_idx_flattened_2/image_width
+    unraveled2 = torch.stack((u_b2, v_b2)).type(torch.float64)
+    uv_b2 = Variable(unraveled2.cuda().squeeze(), requires_grad=True)
+
+    constraint = torch.sqrt((uv_b - uv_b2).pow(2).sum(0)) - (L * d)
+    return constraint 
+
+def local_crop(U, V): 
+    U = U.repeat(8, 1).view(U.shape[0], 8)
+    V = V.repeat(8, 1).view(V.shape[0], 8)
+    x = torch.Tensor([0, 0, -1, -1, -1, 1, 1, 1]).cuda()
+    y = torch.Tensor([-1, 1, 0, 1, -1, 0, 1, -1]).cuda()
+    res_x = U[:,].float() + x
+    res_y = V[:,].float() + y
+    return res_x, res_y
+
+def lipschitz_batch(matches_b, matches_b2, image_a_pred, image_b_pred, L, d, image_width=640, image_height=480):
+    matches_b_descriptor = torch.index_select(image_b_pred, 1, matches_b)
+    matches_b_descriptor = matches_b_descriptor.view(matches_b_descriptor.shape[1], 1, matches_b_descriptor.shape[2])
+    norm_degree = 2
+    image_a_pred_batch = image_a_pred.squeeze().repeat(matches_b.shape[0], 1).view(matches_b.shape[0], image_a_pred.shape[1], image_a_pred.shape[2])
+    descriptor_diffs = image_a_pred_batch - matches_b_descriptor
+    norm_diffs = descriptor_diffs.norm(norm_degree, 2).pow(2)
+    best_match_indices = torch.argmin(norm_diffs, dim=1)
+    U_b = best_match_indices%image_width
+    V_b = best_match_indices/image_width
+    U_b_neighbors, V_b_neighbors = local_crop(U_b,V_b)
+    matches_b2 = U_b_neighbors%image_width + V_b_neighbors*image_width
+    matches_b2 = matches_b2.long().flatten()
+    matches_b2_descriptor = torch.index_select(image_b_pred, 1, matches_b2)
+    print(matches_b2_descriptor.shape)
+
 def get_distributional_loss(image_a_pred, image_b_pred, image_a_mask, image_b_mask,  matches_a, matches_b, bimodal=False):
+    lipschitz_batch(matches_b, None, image_a_pred, image_b_pred, 1, 10)
     masked_indices_a = flattened_mask_indices(image_a_mask, inverse=True)
     masked_indices_b = flattened_mask_indices(image_b_mask, inverse=True)
     reverse_idxs = list(range(len(matches_a)-1, -1, -1))
